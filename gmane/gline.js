6b8b418e1c4c53e18d99778f7504b6b0dac5c3d3
ad-kat <90676982+ad-kat@users.noreply.github.com>
2023-06-21 20:44:25 +0530

    Add files via upload

---
+++ gmane/gline.js
@@ -0,0 +1,3 @@
+gline = [ ['Month','umich.edu','mac.com','unl.edu','columbia.edu','virginia.edu','cam.ac.uk','mtu.edu','gmail.com','unicon.net','berkeley.edu'],
+['2005-12',23,10,8,7,6,5,5,5,5,5]
+];
